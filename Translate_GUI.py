c3bea2623a9c170b9e5e0ff84081f2510a18dac5
SomeZcom <tzaliyun@163.com>
2018-04-05 21:11:17 +0800

    temp

--- Translate_GUI.py
+++ Translate_GUI.py
@@ -3,19 +3,31 @@ from BaiduTranslate import BaiduFanyi
 import time
 
 root = Tk()
-
+root.geometry('800x450+100+100')
+v = StringVar()
+flag = True
 while True:
     try:
         cb = root.clipboard_get()
         print(cb)
         B = BaiduFanyi(cb)
         B.run()
+        means = B.trans_res
+        print(means)
     except:
         print('剪切板为空 or 不是正确的单词')
+    v.set(means)
+    text = Text(root, width=500, height=450)
+    text.insert(1.0, means)
+    text.pack()
     time.sleep(1)
- 
-root.destroy()
+    if flag:
+        root.mainloop()
+        flag = False
     
-root.mainloop()
+ 
+#root.destroy()
+    
+
 
 
